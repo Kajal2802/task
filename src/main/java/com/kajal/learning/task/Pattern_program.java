d873e4575dfa13ccf135c5d6147a19fad739b87e
kajal <kajaldhawale2000@gmail.com>
2024-03-09 13:20:05 +0530

    pattern program

---
+++ src/main/java/com/kajal/learning/task/Pattern_program.java
@@ -0,0 +1,12 @@
+package com.kajal.learning.task;
+
+public class Pattern_program {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 4; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
